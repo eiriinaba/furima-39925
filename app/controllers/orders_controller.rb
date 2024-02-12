213b76892c005edb49b90321a2fe24d3d082189b
eiriinaba <ushinohanako613@gmail.com>
2024-02-13 00:14:15 +0900

    商品購入機能#1-2

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,7 +15,7 @@ class OrdersController < ApplicationController
   def create
     @order_address = OrderAddress.new(order_params)
     @item = Item.find(params[:item_id])
-    if @order_address.valid?s
+    if @order_address.valid?
       pay_item
       @order_address.save
       return redirect_to root_path
