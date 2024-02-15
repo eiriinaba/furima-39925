98b803df1176c61b8c1818b2d7a28bf36bfc3fb5
eiriinaba <ushinohanako613@gmail.com>
2024-02-15 21:49:48 +0900

    商品購入機能#4

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,7 +5,7 @@ class OrderAddress
   with_options presence: true do
     validates :user_id, :item_id, :post_code, :city, :house_number, :phone_number, :token
     validates :region_id, numericality: {other_than: 1, message: "can't be blank"}
-    validates :house_number, format: {with: /\A\d+-?\d+\z/, message: "is invalid. Should be in the format 123-4567 or 1234567"}
+    validates :house_number
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid. Must be 10 or 11 digits"}
   end
